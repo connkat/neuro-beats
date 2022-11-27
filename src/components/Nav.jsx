17d5787e5ee9ce1750de10a27c9e7a62d11a1620
connkat <hi.connkat@gmail.com>
2022-11-26 17:57:54 -0700

    fixed nav mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,29 +6,22 @@ import MobileNav from "./MobileNav";
 import "./Nav.css";
 
 function Nav() {
-  const [state, setState] = useState({
-    mobileView: false,
-  });
-  const { mobileView } = state;
-
-  useEffect(() => {
-    const setResponsiveness = () => {
-      return window.innerWidth < 600
-        ? setState((prevState) => ({ ...prevState, mobileView: true }))
-        : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
-
-    setResponsiveness();
-    window.addEventListener("resize", () => setResponsiveness());
-
-    return () => {
-      window.removeEventListener("resize", () => setResponsiveness());
-    };
-  }, []);
-
   return (
     <div className="Nav">
-   {mobileView ? <MobileNav /> : <DesktopNav />}
+      <div className="nav-logo">
+        <a href="/">
+          <img src="./images/logo-image.png" alt="logo" className="logo" />
+        </a>
+      </div>
+      <div className="title">
+        <h1>NEURO-BEATS</h1>
+        <h3>CREATE MUSIC WITH YOUR BRAIN</h3>
+      </div>
+      <div className="contact">
+        <a href="mailto:hi.connkat@gmail.com" target="_blank" rel="noreferrer">
+          Contact
+        </a>
+      </div>
     </div>
   );
 }
