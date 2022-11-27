4296a58b6a0226117e1df3fb86351d509031acd6
connkat <hi.connkat@gmail.com>
2022-11-26 17:13:55 -0700

    unused vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 import Nav from "./components/Nav";
-import DesktopHome from "./components/DesktopHome";
+// import DesktopHome from "./components/DesktopHome";
 import MobileHome from "./components/MobileHome";
 
 import "./App.css";
 
 function App() {
-  const [state, setState] = useState({
-    mobileView: false,
-  });
-  const { mobileView } = state;
+  // const [state, setState] = useState({
+  //   mobileView: false,
+  // });
+  // const { mobileView } = state;
 
-  useEffect(() => {
-    const setResponsiveness = () => {
-      return window.innerWidth < 600
-        ? setState((prevState) => ({ ...prevState, mobileView: true }))
-        : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
+  // useEffect(() => {
+  //   const setResponsiveness = () => {
+  //     return window.innerWidth < 600
+  //       ? setState((prevState) => ({ ...prevState, mobileView: true }))
+  //       : setState((prevState) => ({ ...prevState, mobileView: false }));
+  //   };
 
-    setResponsiveness();
-    window.addEventListener("resize", () => setResponsiveness());
+  //   setResponsiveness();
+  //   window.addEventListener("resize", () => setResponsiveness());
 
-    return () => {
-      window.removeEventListener("resize", () => setResponsiveness());
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", () => setResponsiveness());
+  //   };
+  // }, []);
   return (
     <div className="App">
       <Nav />
